87b9c48314d901677a5985da80dce5566ae154b5
Mustafa Ünal <mustafaunaldev@gmail.com>
2026-08-21 22:17:52 +0300

    chore: scaffold Swift package for menu bar app

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+    name: "Zwix",
+    platforms: [.macOS(.v13)],
+    targets: [
+        .executableTarget(name: "Zwix", path: "Sources/Zwix")
+    ]
+)
